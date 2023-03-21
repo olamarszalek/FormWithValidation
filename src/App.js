e06f025c7affa45e0562c72e4bb8545a7c973b96
ola marszalek <ola.0608@gmail.com>
2023-03-21 18:50:25 +0100

    final steps

--- src/App.js
+++ src/App.js
@@ -17,34 +17,47 @@ function App() {
       type: "text",
       placeholder: "Username",
       label: "Username",
+      errorMessage: "Username should be 3-16 characters and shouldn't include special characters!",
+      required: true,
+      pattern: "^[A-Za-z0-9]{3,16}$"
     },
     {
       id: 2,
       name: "email",
-      type: "text",
+      type: "email",
       placeholder: "Email",
       label: "Email",
+      errorMessage: "Only valid email addresses!",
+      required: true,
     },
     {
       id: 3,
       name: "birthday",
-      type: "text",
+      type: "date",
       placeholder: "Birthday",
       label: "Birthday",
+      errorMessage: "",
+
     },
     {
       id: 4,
       name: "password",
-      type: "text",
+      type: "password",
       placeholder: "Password",
       label: "Password",
+      errorMessage: "Password should be 8-20 characters and include at least 1 capital letter, 1 number and 1 special character!",
+      required: true,
+      pattern: "^(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=])(?=.{8,20}).*$",
+      
     },
     {
       id: 5,
       name: "confirmPassword",
-      type: "text",
+      type: "password",
       placeholder: "Confirm Password",
       label: "Confirm Password",
+      errorMessage: "Passwords don't match",
+      required: true,
     },
   ];
   const handleSubmit = (e) => {
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,15 +1,27 @@
-import './FormInput.css';
+import "./FormInput.scss";
 
-import React from 'react'
+import React, { useState } from "react";
 
 const FormInput = (props) => {
-const {label, onChange, id, ...inputProps} = props;
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
 
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
   return (
-    <div className='formInput'><label>{label}</label>
-    <input {...inputProps} onChange={onChange} />
+    <div className="formInput">
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() => inputProps.name==="confirmPassword" && setFocused(true)}
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
     </div>
-  )
-}
+  );
+};
 
-export default FormInput
+export default FormInput;
